9cd365461fc680cfa4c3a1a65c1bc325e02aecb7
marko.boger <marko.boger@gmail.com>
2023-12-08 23:56:54 +0100

    Conductor ticks for the first time

--- src/main/scala/de/htwg/scalamusic/Main.scala
+++ src/main/scala/de/htwg/scalamusic/Main.scala
@@ -1,5 +1,30 @@
 package de.htwg.scalamusic
 
+import players._
+import Conductor._
+import music._
+
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+
 @main
 def run(): Unit =
     println("This is ScalaMusic!")
+
+    val system: ActorSystem[Conductor.ConductorCommand] = ActorSystem(Conductor().starttimer(), "ScalaMusic")
+    val conductor: ActorRef[Conductor.ConductorCommand] = system
+    // conductor ! Add(PianoPlayer)
+    // Conductor ! Add(CelloPlayer)
+    // conductor ! Start()
+    conductor ! Tick(1)
+
+    val voice1 = Tune(e2, e2, |, f2, e2, |, g, g, f, e, |, f2, e2)
+    val voice2 = Tune(c, g, c, g, |, a, f, c, g, |, e, e, d, c, |, d, b, c2)
+
+    // PianoPlayer.play(voice1)
+    // CelloPlayer.play(voice2)
+
+    Thread.sleep(4000)
+
+    // Terminate the actor system
+    system.terminate()
--- src/main/scala/de/htwg/scalamusic/players/Conductor.scala
+++ src/main/scala/de/htwg/scalamusic/players/Conductor.scala
@@ -1,11 +1,12 @@
 package de.htwg.scalamusic.players
 
 import akka.actor.typed.{Behavior, ActorSystem, ActorRef, Props}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{Behaviors, TimerScheduler}
 import akka.actor.Scheduler
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import de.htwg.scalamusic.music._
+import scala.concurrent.duration._
 
 object Conductor:
     sealed trait ConductorCommand
@@ -16,12 +17,28 @@ object Conductor:
     final case class PlayAt(tick: Int, music: List[Option[Music]]) extends ConductorCommand
     final case class Add(player: MusicPlayer) extends ConductorCommand
 
-    def apply(): Behavior[ConductorCommand] =
-        Behaviors.withTimers(timers => new Conductor(List.empty).conduct(List.empty))
+    // def apply(): Behavior[ConductorCommand] =
+    //     Behaviors.withTimers(timers => new Conductor(List.empty).conduct(List.empty))
 
 case class Conductor(players: List[MusicPlayer] = List.empty):
     import Conductor._
-    private def conduct(players: List[MusicPlayer]): Behavior[ConductorCommand] = {
+
+    def starttimer(): Behavior[ConductorCommand] =
+        Behaviors.withTimers { timers =>
+            timers.startTimerWithFixedDelay(Tick(1), Tick(1), 50.millisecond)
+
+            Behaviors.receive { (context, message) =>
+                message match {
+                    case Tick(tickNum: Int) =>
+                        println("tick " + tickNum)
+                        // Schedule a one-time task after a delay
+                        // timers.startSingleTimer(Tick(1), Tick(1), 3.seconds)
+                        Behaviors.same
+                }
+            }
+        }
+
+    def conduct(players: List[MusicPlayer]): Behavior[ConductorCommand] = {
         Behaviors.receiveMessage {
             case Start() =>
                 println("Conductor Started")
